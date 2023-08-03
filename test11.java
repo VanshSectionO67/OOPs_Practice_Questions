a190c52de7cb4aef1e17ff108023febfe5247ed1
VanshSectionO(67) <122622923+VanshSectionO67@users.noreply.github.com>
2023-08-03 18:41:06 +0530

    Add files via upload

---
+++ test11.java
@@ -0,0 +1,11 @@
+class test11
+{
+    public static void main(String args[])
+    {
+        int num=8;
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i+" "+"* "+num+" "+"= "+i*num);
+        }
+    }
+}
