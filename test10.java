365b0f9f08ca1a783ffc85e9fdefe05f8690884b
VanshSectionO(67) <122622923+VanshSectionO67@users.noreply.github.com>
2023-08-03 17:16:50 +0530

    Add files via upload

---
+++ test10.java
@@ -0,0 +1,21 @@
+class test10
+{
+    public static void main(String args[])
+    {
+        int num=1234,rem=0,rem2=0;
+        while(num!=0)
+        {
+            rem=num%10;
+            break;
+        }
+        int last=rem;
+        while(num!=0)
+        {
+            rem2=num%10;
+            num=num/10;
+        }
+        int first=rem2;
+        System.out.println("First digit of no is "+first);
+        System.out.println("Last digit of no is "+last);
+    }
+}
