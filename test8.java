3460ec2e5fe2fb166cc41c0a2cdcc3a04056f9f7
VanshSectionO(67) <122622923+VanshSectionO67@users.noreply.github.com>
2023-08-03 16:57:58 +0530

    Add files via upload

---
+++ test8.java
@@ -0,0 +1,12 @@
+class test8
+{
+    public static void main(String args[])
+    {
+        int num=9,fact=1;
+        for(int i=1;i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("Factorial of no is "+fact);
+    }
+}
